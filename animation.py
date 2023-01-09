2525ab4438fef59c64e6d7606019def6e4fcda87
Gibs13 <gsgwlol@gmail.com>
2023-01-09 08:11:01 +0100

    comment + UE udp call tests

---
+++ animation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  25 08:42:49 2022
+
+@author: Gsgwl
+"""
+import socket
+
+UDP_IP = "127.0.0.2"
+UDP_PORT = 5006
+MESSAGE = "Hello, World!"
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+except socket.gaierror:
+    print("error sending packet")
--- launch.py
+++ launch.py
@@ -11,22 +11,36 @@ from tkinter import *
 from tkinter import filedialog
 from dlclive import benchmark
 
+'''
 modelpath = ''
 filepath = ''
+
+
+Frame from camera -> dlclive.processframe -> dlclive.init_inference -> dlclive.getpose
+
+out :
+Pose
+
+-> 2d to 3d (voir avec Loann)
+
+'''
+
 
 def open_video():
     global filepath
     filepath = filedialog.askopenfilename(title="Selectionner une video", filetypes=(("file_type","*.extension"),("all files","*.*")))
-   
    
 def open_mod():
     global modelpath
     modelpath = filedialog.askdirectory(title="Selectionner un modele")
       
 def launch():
-    benchmark(modelpath,filepath,display=True,pixels=45000)
+    benchmark(modelpath,filepath,display=True,pixels=45000,pcutoff=scale.get())
   
 window=Tk()
+
+scale=DoubleVar()
+
 Label(window, text="Selectionner", font='Arial 16 bold').pack(pady=15)
 
 button_video = Button(window, text="Choisir video", command=open_video)
@@ -35,5 +49,14 @@ button_modele = Button(window, text="Choisir modele", command=open_mod)
 button_modele.pack()
 button_launch = Button(window, text="Lancer", command=launch)
 button_launch.pack()
+
+Scale(
+    master=window,
+    from_=0,
+    to=1,
+    resolution=0.05,
+    orient=HORIZONTAL,
+    variable=scale
+).pack(side=BOTTOM)
 
 window.mainloop()
